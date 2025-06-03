77ff716dda2f61a615fb8bdde1dc314ec62ee427
jh092614 <jh092614@gmail.com>
2025-06-03 01:54:20 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'IFTA MILE TRACKER - Web App'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
