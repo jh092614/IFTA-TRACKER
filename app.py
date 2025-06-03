d4e690d6fda530779a8cc5a5385526d7589eb8a0
jh092614 <jh092614@gmail.com>
2025-06-02 23:32:15 -0700

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 def home():
     return 'IFTA MILE TRACKER - Web App'
 
-# ✅ Required for Render: bind to 0.0.0.0 and use PORT env variable
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
